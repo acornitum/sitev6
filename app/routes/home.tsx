319ff9810395ac904569ae9a88ec231f210ab0cb
acon <101356372+acornitum@users.noreply.github.com>
2025-12-09 22:12:18 -0500

    change favicon

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "acon" },
+    { name: "description", content: "acon's personal site, v6 >:3" },
   ];
 }
 
