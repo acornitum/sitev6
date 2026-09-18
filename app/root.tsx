084e137d4249ab6cc0ad73d0648f6c3dc8001042
acon <101356372+acornitum@users.noreply.github.com>
2026-09-18 00:13:02 -0400

    add analytics

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,7 @@ import type { Route } from "./+types/root";
 import "./app.css";
 
 import dotenv from 'dotenv';
+import { Analytics } from '@vercel/analytics/react';
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -43,6 +44,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
+        <Analytics />
       </body>
     </html>
   );
